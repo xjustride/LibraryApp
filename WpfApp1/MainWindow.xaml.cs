661f41170f167ca1094f753f8353478cefe4e6eb
xjustride <remigiusz.krupa@microsoft.wsei.edu.pl>
2023-05-23 22:55:50 +0200

    updating interface and adding some functions to buttons

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,25 +1,56 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 
 namespace WpfApp1
 {
 	public partial class MainWindow : Window
 	{
+		private List<Contact> contacts;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			contacts = new List<Contact>();
+			InitializeContacts();
+			ContactsGrid.ItemsSource = contacts;
 		}
 
-		private void Button_Click(object sender, RoutedEventArgs e)
+		private void InitializeContacts()
 		{
-            // Kod obslugi zdarzenia
-        }
-
-		private void Button_Click_1(object sender, RoutedEventArgs e)
-		{
-			// Kod obsługi zdarzenia
+			// Przykładowe dane kontaktów
+			contacts.Add(new Contact { FirstName = "Jan", LastName = "Kowalski", PhoneNumber = "123456789", Email = "jan.kowalski@example.com" });
+			contacts.Add(new Contact { FirstName = "Anna", LastName = "Nowak", PhoneNumber = "987654321", Email = "anna.nowak@example.com" });
+			// Dodaj więcej kontaktów lub pobierz z bazy danych
 		}
+
+		private void AddContactButton_Click(object sender, RoutedEventArgs e)
+		{
+			// Logika dodawania nowego kontaktu
+			// Możesz otworzyć nowe okno dialogowe lub formularz do wprowadzania danych
+			// Po dodaniu kontaktu, zaktualizuj listę kontaktów i odśwież widok DataGrid
+		}
+
+		private void EditContactButton_Click(object sender, RoutedEventArgs e)
+		{
+			// Logika edycji istniejącego kontaktu
+			// Możesz otworzyć okno dialogowe lub formularz z wybranymi danymi kontaktu do edycji
+			// Po zapisaniu zmian, zaktualizuj listę kontaktów i odśwież widok DataGrid
+		}
+
+		private void DeleteContactButton_Click(object sender, RoutedEventArgs e)
+		{
+			// Logika usuwania kontaktu
+			// Pobierz zaznaczony kontakt z DataGrid i usuń go z listy kontaktów
+			// Po usunięciu, odśwież widok DataGrid
+		}
+	}
+
+	public class Contact
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string PhoneNumber { get; set; }
+		public string Email { get; set; }
 	}
 }
