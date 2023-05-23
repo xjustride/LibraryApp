7890712a0689707060f38bbb47818392c46d3081
xjustride <remigiusz.krupa@microsoft.wsei.edu.pl>
2023-05-23 22:37:28 +0200

    Initial commit

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+            // Kod obslugi zdarzenia
+        }
+
+		private void Button_Click_1(object sender, RoutedEventArgs e)
+		{
+			// Kod obsługi zdarzenia
+		}
+	}
+}
